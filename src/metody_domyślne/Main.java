99dee76da8d5900a16834bb87cbb259e129a336c
Gumisia <gabrielaprymaka@icloud.com>
2018-05-10 17:11:24 +0200

    metody domyslne

---
+++ src/metody_domyślne/Main.java
@@ -0,0 +1,41 @@
+package metody_domyślne;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Formula f = (int a) -> {
+              return Math.sqrt(a)*2;
+        };
+
+        System.out.println(f.calculate(2));
+        System.out.println(f.sqrt(3));
+
+        A test = new A(5);
+        System.out.println(test.calculate(test.a));
+
+
+    }
+
+}
+
+interface Formula{
+    double calculate(int a); // musi być jedna metoda domyślna na dany interfejs
+
+    default double sqrt(int a){
+        return Math.sqrt(a);
+    }
+}
+
+class A implements Formula{
+
+    int a;
+
+    A(int a){
+        this.a = a;
+    }
+
+    @Override
+    public double calculate(int a) {
+        return this.sqrt(a * 5);
+    }
+}
